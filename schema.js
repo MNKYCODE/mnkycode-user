9d23e49078612058090e85053afc2299020fc72a
Lars Hampe <lars@mnkycode.com>
2015-09-24 21:55:45 +0200

    Add user rights to schema

--- schema.js
+++ schema.js
@@ -9,7 +9,23 @@ var user = new Schema({
   email: String,
   password: String,
   token: String,
-  signup: { type: Date, default: Date.now() }
+  signup: { type: Date, default: Date.now() },
+  rights: {
+    user: {
+      login: { type: Boolean, default: true },
+      dashboard: { type: Boolean, default: true },
+      list: { type: Boolean, default: false },
+      delete: { type: Boolean, default: false },
+      create: { type: Boolean, default: false },
+      edit: { type: Boolean, default: false }
+    },
+    blog: {
+      dashboard: { type: Boolean, default: false },
+      create: { type: Boolean, default: false },
+      list: { type: Boolean, default: false },
+      edit: { type: Boolean, default: false }
+    }
+  }
 });
 
 module.exports = mongoose.model('users', user);
